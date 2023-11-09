3ddcf0b9ea781b7163df6a24f2e0eb4f98beb8a4
Mlaurgaard <m.laurgaard@gmail.com>
2023-11-09 13:04:20 +0100

    Created partner.js
    JS
    - Added object array for makeshift partners

---
+++ javascript/partners.js
@@ -0,0 +1,26 @@
+let myPartners =[
+    {
+        firstName: "InnovateTech Solutions",
+        image: "",
+        description:"Tech Powerhouse, Cutting-edge software and hardware solutions",
+        email:"contact@innovatetechsolutions.com",
+        phone:"+1 (555) 123-4567",
+    },
+    {
+        firstName: "Celestial Marketing Agency",
+        image: "",
+        description:"Digital Marketing Experts, Strategic campaigns for a stellar online presence",
+        email:"info@celestialmarketingagency.com",
+        phone:"+1 (555) 987-6543",
+    },
+    {
+        firstName: "Harmony Analytics Group",
+        image: "",
+        description:"Data Wizards, Insights and recommendations for optimal user experience",
+        email:"contact@harmonyanalyticsgroup.com",
+        phone:"+1 (555) 876-5432",
+    },
+];
+
+
+
