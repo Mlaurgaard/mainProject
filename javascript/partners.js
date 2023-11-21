5d0aaaa41b1927442decf44e2649630931ef5316
Mlaurgaard <m.laurgaard@gmail.com>
2023-11-21 11:36:23 +0100

    Partner cards update
    HTML:
    - added div for cards
    
    CSS:
    Partners.css
      -Added button and description class for cards specifically.
    
    Utility.css
      - Added more margin and padding classes.
    
    JS:
    - Added rest of card-print function
    - temporarily removed some features due to reconsideration of design.
    - Started working on modal window.

--- javascript/partners.js
+++ javascript/partners.js
@@ -1,30 +1,30 @@
 let myPartners = [
     {
         name: "InnovateTech Solutions",
-        image: "",
-        description:"Tech Powerhouse, Cutting-edge software and hardware solutions",
+        image: "https://www.mrinmaya.io/images/teampic/na.jpg",
+        description:"Tech Powerhouse, A Cutting-edge software and hardware solutions",
         email:"contact@innovatetechsolutions.com",
         phone:"+1 (555) 123-4567",
     },
     {
         name: "Celestial Marketing Agency",
-        image: "",
+        image: "https://www.mrinmaya.io/images/teampic/na.jpg",
         description:"Digital Marketing Experts, Strategic campaigns for a stellar online presence",
         email:"info@celestialmarketingagency.com",
         phone:"+1 (555) 987-6543",
     },
     {
         name: "Harmony Analytics Group",
-        image: "",
+        image: "https://www.mrinmaya.io/images/teampic/na.jpg",
         description:"Data Wizards, Insights and recommendations for optimal user experience",
         email:"contact@harmonyanalyticsgroup.com",
         phone:"+1 (555) 876-5432",
     },
 ];
 
-// Open modal function
+//function to display the partner cards
 function displayPartnerCards(){
-    let partnerWindow = document.getElementById('');
+    let partnerWindow = document.getElementById('partnerCards');
     myPartners.forEach(partner =>{
         partnerWindow.appendChild(createPartnerCard(partner));
     });
@@ -36,23 +36,23 @@ function displayPartnerCards(){
 function createPartnerCard(partner){
     let card = document.createElement("div");
 
-    card.classList = "add-flex card-container flexDir-col ml-1r bgclr-3";
+    card.classList = "add-flex card-container flexDir-col ml-1r bgclr-3 cardBorder";
     card.onclick = function(){
-        displayPartnerCards(partner);
+        // displayPartnerCards(partner);
     };
 
     let partnerName = document.createElement("h2");
     partnerName.textContent = partner.name;
-    partnerName. classList = "";
+    partnerName. classList = "as-center bgclr-4 mt-auto";
 
     let partnerImage = document.createElement("img");
     partnerImage.src = partner.image;
     partnerImage.alt = partner.name;
-    partnerImage.style = "";
+    partnerImage.style = "height: 12rem; width: 13.7rem;";
 
     let partnerDescription = document.createElement("p");
     partnerDescription.textContent = partner.description;
-    partnerDescription.classList = "";
+    partnerDescription.classList = "cardDescription";
 
     let partnerEmail = document.createElement("p");
     partnerEmail.textContent = partner.email;
@@ -62,14 +62,22 @@ function createPartnerCard(partner){
     partnerPhone.textContent = partner.phone;
     partnerPhone.classList = "";
 
+    let infoButton = document.createElement("button");
+    infoButton.textContent = "Open";
+    infoButton.classList = "cardButton";
+
     card.appendChild(partnerImage);
     card.appendChild(partnerName);
-    card.appendChild(partnerDescription);
-    card.appendChild(partnerEmail);
-    card.appendChild(partnerPhone);
+    // card.appendChild(partnerDescription);
+    card.appendChild(infoButton);
+    // card.appendChild(partnerEmail);
+    // card.appendChild(partnerPhone);
 
     return card;
-};
+}
+
+
+
 
 // // myPartnerCards()
     // Connect to HTMl div, to push cards
@@ -78,8 +86,24 @@ function createPartnerCard(partner){
 
 
 
-// Function for opening modal
+// Function for opening modal and display partners
+function openPartnerModal(partner){
+    let modalWindow = document.getElementById("");
+    let modalBackground = document.getElementById("");
+    modalWindow.style.display = "flex";
+    modalBackground.classList = ("");
 
+    // function for picture in modal
+
+    document.getElementById("").textContent = partner.name;
+    document.getElementById("").textContent = partner.image;
+    document.getElementById("").textContent = partner.description;
+    document.getElementById("").textContent = partner.phone;
+    document.getElementById("").textContent = partner.email;
+}
+
+
+displayPartnerCards();
 // Opening modal shows object name, desc, email and phone
 
 // 
